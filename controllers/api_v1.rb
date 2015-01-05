b705793b6803b2dbe3ab2c1eb0ad85b2e70a13b1
stujo <stujo@users.noreply.github.com>
2015-01-05 10:44:19 -0800

    12-playlist-mvc-for-CRUD-SPA

--- controllers/api_v1.rb
+++ controllers/api_v1.rb
@@ -23,3 +23,30 @@ post '/api/v1/songs/new' do
     halt 400, {errors: @song.errors}.to_json
   end
 end
+
+
+post '/api/v1/song/:id' do
+  begin
+    song_params = params[:song] || {}
+    @song = Song.find(params[:id])
+    if @song.update_attributes(song_params)
+      @song.to_json
+    else
+      halt 400, {errors: @song.errors}.to_json
+    end
+
+  rescue ActiveRecord::RecordNotFound => e
+    halt 404, {error: "Song Not Found"}.to_json
+  end
+end
+
+
+delete '/api/v1/song/:id' do
+  begin
+    @song = Song.find(params[:id])
+    @song.destroy
+    204
+  rescue ActiveRecord::RecordNotFound => e
+    halt 404
+  end
+end
