4b295816848878691172b681ee5315cf28cfcb20
stujo <stujo@users.noreply.github.com>
2015-01-05 10:35:11 -0800

    03-playlist-get-with-error-handling

--- controllers/api_v1.rb
+++ controllers/api_v1.rb
@@ -5,3 +5,11 @@ end
 get '/api/v1/songs' do
   Song.all.to_json
 end
+
+get '/api/v1/song/:id' do
+  begin
+    Song.find(params[:id]).to_json
+  rescue ActiveRecord::RecordNotFound
+    halt 404, { error: 'Song Not Found' }.to_json
+  end
+end
